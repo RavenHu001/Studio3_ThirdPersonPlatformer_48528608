867e02354c4946e5396a316fd8511edfcc2ac913
RavenHu001 <jhu45@student.ubc.ca>
2025-02-19 15:49:22 -0800

    implement the score add function

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,11 +4,21 @@ using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private float Score=0f;
+    [SerializeField] private float score=0f;
     private CollisionTrigger[] coins;
 
     public void Start()
     {
         coins = FindObjectsByType<CollisionTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        foreach (CollisionTrigger coin in coins)
+        {
+            coin.getCoin.AddListener(IncrementScore);
+        }
     }
+
+    private void IncrementScore() 
+    {
+        score++;
+    }
+    
 }
