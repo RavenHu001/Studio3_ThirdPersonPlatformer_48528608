6f6c2228a75dfd060eb027d18e649d4f90b3ba6f
RavenHu001 <jhu45@student.ubc.ca>
2025-02-19 15:18:53 -0800

    finish the collision detector of coin

---
+++ Assets/Script/CollisionTrigger.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisionTrigger : MonoBehaviour
+{
+    public UnityEvent getCoin = new();
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        { 
+            getCoin?.Invoke();
+            Debug.Log("Get coin");
+            Destroy(gameObject);
+        }
+    }
+}
