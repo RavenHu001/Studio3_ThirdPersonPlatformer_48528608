84dd9fe74d4646ba76163306100679c80300945c
RavenHu001 <jhu45@student.ubc.ca>
2025-02-19 16:56:31 -0800

    implement double jump function

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,13 +8,23 @@ public class PlayerControl : MonoBehaviour
 
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float doubleJumpForce;
 
-    private Rigidbody rb;
+    private int maxJumpCount = 2;
+    private int jumpCount = 0;
+    private int maxDashCount = 1;
+    private int dashCount = 0;
+    private float height;
     private bool isGrounded;
+    private Rigidbody rb;
+    private CapsuleCollider playerCollider;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //get the height of player
+        playerCollider = GetComponent<CapsuleCollider>();
+        height = playerCollider.height/2;
         //get RB
         rb = GetComponent<Rigidbody>();
         //add listener
@@ -39,36 +49,33 @@ public class PlayerControl : MonoBehaviour
     //apply y move of player
     private void JumpPlayer(Vector3 direction) 
     {
-        if (canJump()&&direction.y>0)
+        //call check ground
+        CheckGrounded();
+        if (jumpCount<maxJumpCount)
         {
+            //choose the force of jump based on jumpCount
+            float force = (jumpCount != maxJumpCount) ? jumpForce : doubleJumpForce;
+            // reset the y axis velocity
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
             //Debug.Log("jump");
             Vector3 moveDirection = new(0, direction.y, 0);
             rb.AddForce(moveDirection * jumpForce, ForceMode.Impulse);
+            //decrease jump count
+            jumpCount++;
         }
     }
 
     //method to check can or cannot jump
-    private bool canJump() 
+    private void CheckGrounded() 
     {
-        if(isGrounded) return true;
-        return false;
+        //change to lazer checker
+        float rayLength = height+0.01f;
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, rayLength);
+
+        if (isGrounded)
+        {
+            jumpCount = 0;//reset jumpCount
+        }
     }
 
-    //collision checker
-    void OnCollisionEnter(Collision collision)
-    {
-        // Check if it collides with the ground
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-    void OnCollisionExit(Collision collision)
-    {
-        // Leave ground then update
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
-    }
 }
